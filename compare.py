134902efb5c42bd9c44561c9144ca355701e598a
Zac-HD <zac.hatfield.dodds@gmail.com>
2016-10-15 19:22:28 +1100

    Sort output, cheap compare for testing

--- compare.py
+++ compare.py
@@ -8,7 +8,7 @@ def compress_str(string_in,dump_thes=1):
     #convert to unicode
     cmp = unidecode.unidecode(string_in)
     #dump puncuation and white spaces
-    cmp = "".join(s for s in cmp if s not in string.punctuation+' ')
+    cmp = "".join(s for s in cmp if s not in string.punctuation+' ').lower()
     if dump_thes:
         cmp = cmp.replace('the', '')
     return cmp
@@ -23,9 +23,13 @@ def compare(input_list):
     div = [[input_list[0]]]
     for inp in input_list[1:]:
         for divj in div:
-            cmp_score = distance.sorensen(inp.cmp_title, divj[0].cmp_title)
-            if cmp_score < 0.2:  #add duplicate
+            divj_title = divj[0].cmp_title
+            a, b = set(divj_title), set(inp.cmp_title)
+            cmp_score = len(a&b) / len(a|b)
+            #print(cmp_score)
+            if cmp_score > 0.95:  #add duplicate
                 divj.append(inp)
                 break
+        else:
             div.append([inp])
     return div
--- dedupe.py
+++ dedupe.py
@@ -1,5 +1,4 @@
 #! python3
-
 import collections
 import itertools
 import sys
@@ -7,35 +6,42 @@ import sys
 import btx_io, choose_best, compare
 
 
+def key_func(entry):
+    """Return a key for the group this entry falls into."""
+    try:
+        year = int(entry.fields.get('year'))
+    except:
+        year = -1
+    try:
+        return year, int(entry.fields.get('number'))
+    except:
+        return year, -1
+
+
 def group_entries(entries):
     """Takes an iterable of entries, and returns list of lists, where all
     potentially-duplicated entries should be in the same sublist.
     """
-    def key_func(entry):
-        """Return a key for the group this entry falls into."""
-        try:
-            year = int(entry.fields.get('year'))
-        except:
-            year = None
-        try:
-            return year, int(entry.fields.get('number'))
-        except:
-            return year, None
-
     grouper = collections.defaultdict(list)
     for e in entries:
         grouper[key_func(e)].append(e)
-    return sorted(grouper.values(), key=len)
+    return sorted(grouper.values(), key=len, reverse=True)
 
 
 def main(*filenames):
     """Do the things."""
     out = []
-    for i, grp in enumerate(
-            group_entries(btx_io.read_bib_entries(*filenames))):
+    inp = list(btx_io.read_bib_entries(*filenames))
+    len_in = len(inp)
+    for i, grp in enumerate(group_entries(inp)):
+        #print(len(grp))
         as_list_list = compare.compare(grp)
+        #print(len(as_list_list))
         out.extend(map(choose_best.best_entry, as_list_list))
-    btx_io.write_bib_entries(out, fname='test_out.bib')
+        if i > 1000:
+            break
+    print(len_in, len(out))
+    btx_io.write_bib_entries(sorted(out, key=key_func), fname='test_out.bib')
 
 
 if __name__ == '__main__':
