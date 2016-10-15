526f4d0cbcfd7c6c4b5be6a3c828a4d69ecbb5df
Zac-HD <zac.hatfield.dodds@gmail.com>
2016-10-15 20:33:46 +1100

    Title-based deduplication

--- dedupe.py
+++ dedupe.py
@@ -6,42 +6,28 @@ import sys
 import btx_io, choose_best, compare
 
 
-def key_func(entry):
-    """Return a key for the group this entry falls into."""
-    try:
-        year = int(entry.fields.get('year'))
-    except:
-        year = -1
-    try:
-        return year, int(entry.fields.get('number'))
-    except:
-        return year, -1
+def title_key(entry):
+    return compare.compress_str(entry.fields.get('title', ''))
 
 
-def group_entries(entries):
-    """Takes an iterable of entries, and returns list of lists, where all
-    potentially-duplicated entries should be in the same sublist.
-    """
-    grouper = collections.defaultdict(list)
-    for e in entries:
-        grouper[key_func(e)].append(e)
-    return sorted(grouper.values(), key=len, reverse=True)
+def dump_titles(*filenames):
+    """Do the things."""
+    with open('titles.txt', 'w', encoding='latin1') as f:
+        out = sorted(compare.compress_str(e.fields.get('title', '')) for e in
+                     btx_io.read_bib_entries(*filenames))
+        f.write('\n'.join(out))
 
 
 def main(*filenames):
     """Do the things."""
-    out = []
-    inp = list(btx_io.read_bib_entries(*filenames))
-    len_in = len(inp)
-    for i, grp in enumerate(group_entries(inp)):
-        #print(len(grp))
-        as_list_list = compare.compare(grp)
-        #print(len(as_list_list))
-        out.extend(map(choose_best.best_entry, as_list_list))
-        if i > 1000:
-            break
-    print(len_in, len(out))
-    btx_io.write_bib_entries(sorted(out, key=key_func), fname='test_out.bib')
+    grouped = collections.defaultdict(list)
+    in_ = list(btx_io.read_bib_entries(*filenames))
+    for e in in_:
+        grouped[title_key(e)].append(e)
+    out = sorted([choose_best.best_entry(g)
+                  for g in grouped.values()], key=title_key)
+    print(len(in_), len(out))
+    btx_io.write_bib_entries(out, fname='test_out.bib')
 
 
 if __name__ == '__main__':
